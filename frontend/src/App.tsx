1f1929de6b480f9acbed6cfd1aabc83d9ba4823c
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-01-09 15:17:43 +0200

    refactor: migrated routes to routerProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,19 @@
 import './App.css';
-import styled, { ThemeProvider } from 'styled-components';
-import { Routes, Route } from 'react-router-dom';
-import { theme } from './assets/app-theme';
-import { GlobalStyles } from './assets/globalStyles';
-import { OptionMenu } from './components/OptionMenu';
-import { PageContainer } from './components/PageContainer';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ListLayout } from './pages/ListLayout';
 import { ListSelection } from './pages/ListSelection';
 import { SelectedList } from './pages/SelectedList';
-import { ListProvider } from './context/listContext';
-import { Device } from './assets/breakpoints';
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <ListProvider>
-        <StyledApp>
-          <GlobalStyles />
-          <PageContainer>
-            <Routes>
-              <Route path="/" element={<ListSelection />} />
-              <Route path="list">
-                <Route path=":id" element={<SelectedList />} />
-              </Route>
-            </Routes>
-          </PageContainer>
-          <OptionMenu />
-        </StyledApp>
-      </ListProvider>
-    </ThemeProvider>
-  );
+const router = createBrowserRouter([
+  {
+    Component: ListLayout,
+    children: [
+      { path: '/', Component: ListSelection },
+      { path: 'list/', children: [{ path: ':id', Component: SelectedList }] },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-const StyledApp = styled.div`
-  display: grid;
-  grid-template-rows: 1fr auto;
-
-  width: min(100vw, 30rem);
-  height: 100svh;
-  background: ${(props) => props.theme.colors.main};
-
-  @media (min-width: ${Device.sm}) {
-    margin-top: 2rem;
-    height: 80svh;
-  }
-`;
-
-export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './assets/app-theme';
+import { GlobalStyles } from './assets/globalStyles.ts';
+import { ListProvider } from './context/listContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <ListProvider>
+        <GlobalStyles />
+        <App />
+      </ListProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
