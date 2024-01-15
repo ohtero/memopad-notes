c34eeed017d379c92b2f01d26beb664c9fe04908
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-01-15 15:00:28 +0200

    refactor: changed route creation method

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from 'react-router-dom';
 import { ListLayout } from './pages/ListLayout';
 import { ListSelection } from './pages/ListSelection';
 import { SelectedList } from './pages/SelectedList';
 
-const router = createBrowserRouter([
-  {
-    Component: ListLayout,
-    children: [
-      { path: '/', Component: ListSelection },
-      { path: 'list/', children: [{ path: ':id', Component: SelectedList }] },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<ListLayout />}>
+      <Route path="/" element={<ListSelection />} />
+      <Route path="list">
+        <Route path=":id" element={<SelectedList />} />
+      </Route>
+    </Route>
+  )
+);
 
 export default function App() {
   return <RouterProvider router={router} />;
