8f725c202a08603bfc8ab2b67cbcbd6408f69dd7
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-01-31 13:23:20 +0200

    bugfix: fix typeguards to properly check for type

--- frontend/src/utils/typeGuard.ts
+++ frontend/src/utils/typeGuard.ts
@@ -1,13 +1,19 @@
 import { ListData, ListItem } from '../typings/types';
 
+type ListTypes = ListData[] | ListItem[];
+
 export function isString(value: unknown): value is string {
   return typeof value === 'string';
 }
 
-export function isList(value: ListData[] | string): value is ListData[] {
-  return value !== undefined;
+export function isList(value: ListTypes): value is ListData[] {
+  return Array.isArray(value) && value.every((item) => typeof item === 'object' && 'list_id' in item);
 }
 
-export function isListValues(value: ListItem[]): value is ListItem[] {
-  return value !== undefined;
+export function isListValue(value?: ListItem): value is ListItem {
+  return value?.list_item_id !== undefined;
+}
+
+export function isListValueArray(value: ListTypes): value is ListItem[] {
+  return Array.isArray(value) && value.every((item) => 'list_item_id' in item);
 }
