7178aba59d266ef0262c97bc9982731c33c1cd4b
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-01-31 13:24:18 +0200

    refactor: move types to centralized file

--- frontend/src/typings/types.d.ts
+++ frontend/src/typings/types.d.ts
@@ -8,3 +8,27 @@ export interface ListItem {
   list_item_value: string;
   completed: boolean;
 }
+
+export type useFetchType = {
+  data: ListData[] | ListItem[] | [];
+  isPending: boolean;
+  error: string | null;
+  setFetchParams: (newParams: FetchParams) => void;
+};
+
+export type FetchParams = {
+  method?: string;
+  operation?: keyof ApiOperations;
+  body?: ListItem;
+  listId?: number;
+  itemId?: string;
+};
+
+export type ApiOperations = {
+  getLists: string;
+  getSingleList: string;
+  deleteList: string;
+  deleteListItem: string;
+  createList: string;
+  postListItem: string;
+};
