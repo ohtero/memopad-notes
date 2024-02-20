fc1b228b1073d180f4bf55e01c81aabe888ca155
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-02-20 13:45:52 +0200

    feature: create button for adding new lists

---
+++ frontend/src/components/AddNewListButton.tsx
@@ -0,0 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+import { modifyRequest as newListRequest } from '../utils/modifyRequest';
+import { MenuButton } from './UI/Button';
+
+type AddNewListButtonProps = {
+  handleClick: () => void;
+};
+
+type ListId = {
+  list_id: number;
+};
+
+export default function AddNewListButton({ handleClick }: AddNewListButtonProps) {
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  const navigate = useNavigate();
+
+  async function createNewList() {
+    const res = await newListRequest<ListId>(`${apiUrl}/lists`);
+    if (!res.success) {
+      console.log(res.error);
+    }
+    if (res.data) {
+      const id = res.data.list_id;
+      navigate(`/list/${id}`, { state: { listId: id, listName: 'Uusi lista', showInput: true } });
+      handleClick();
+    }
+  }
+
+  return <MenuButton handleClick={() => void createNewList()}>Add list</MenuButton>;
+}
